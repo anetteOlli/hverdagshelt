b74229e4f515e380ad78d18ed60672ddfef27f74
larssorl <larssorl@stud.ntnu.no>
2019-01-08 19:05:29 +0100

    Made simple mailController for sending activationlink and mass emails

--- server/src/routes/index.js
+++ server/src/routes/index.js
@@ -1,6 +1,6 @@
 // @flow
 const router = require('express').Router();
-import { checkAuth, genToken } from '../util';
+import { checkAuth, genToken } from '../services/util';
 
 /*--- When the user refreshes the site give the user a new jwt-token and the user info ---*/
 router.get('/refresh', checkAuth, (req: () => mixed, res: express$Response) => {
