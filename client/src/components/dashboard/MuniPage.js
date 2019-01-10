25043dca40c245f01f440f5312777f295375405c
elisamop <elisamop@stud.ntnu.no>
2019-01-10 16:00:17 +0100

    Nå fungerer grid og cards for arrangementer

--- client/src/components/dashboard/MuniPage.js
+++ client/src/components/dashboard/MuniPage.js
@@ -1,7 +1,7 @@
 // @flow
 import React from 'react';
 import withRoot from '../../withRoot';
-import { withStyles, Card, CardContent, Paper, Grid, Typography, TextField, MenuItem, Button, Tabs, Tab, AppBar, NoSsr } from '@material-ui/core';
+import { withStyles, Card, CardContent, CardMedia, CardActionArea, CardActions, Paper, Grid, Typography, TextField, MenuItem, Button, Tabs, Tab, AppBar, NoSsr } from '@material-ui/core';
 import PropTypes from 'prop-types';
 
 import createHashHistory from 'history/createHashHistory';
@@ -46,9 +46,61 @@ const styles = theme => ({
   tabRoot: {
     marginTop: 100,
     marginBottom: 30
+  },
+  card: {
+    margin: 5,
+   [theme.breakpoints.up('sm')]: {
+     maxWidth: 345,
+     minWidth: 345
+   },
+   [theme.breakpoints.down('xs')]: {
+     maxWidth: 200,
+     minWidth: 10
+   }
+  },
+  media: {
+    objectFit: 'cover',
+  },
+  statustext: {
+    color: "#ff9800",
   }
 
 });
+
+/**Event replacement*/
+const events = [
+  {
+    event_id: 0,
+    event_name: "Konsert på tunet",
+    event_description: "brabrabrabrabrabberbeewkfnefleirjglekrnfrlgjntkjtnfreregjernkjgnerkjgnrefkm v,djfenwlrkgmlrkgmvf,md r,kwelwøkflvbktmb",
+    date_starting: "12-14-2356",
+    date_ending: "12-14-2356",
+    status_fk: "KOMMER!",
+    location_fk: "tunet",
+    event_img: "https://proxy.duckduckgo.com/iu/?u=https%3A%2F%2Ftse1.mm.bing.net%2Fth%3Fid%3DOIP.VigDKUTomMIUj8h6FODmWgHaFS%26pid%3D15.1&f=1"
+    },
+  {
+    event_id: 1,
+    event_name: "Høytlesning på biblioteket",
+    event_description: "brabrabrabrabrabberbeewkfnefleirjglekrnfrlgjntkjtnfreregjernkjgnerkjgnrefkm v,djfenwlrkgmlrkgmvf,md r,kwelwøkflvbktmb",
+    date_starting: "12-14-2356",
+    date_ending: "12-14-2356",
+    status_fk: "FOREGÅR NÅ!",
+    location_fk: "biblioteket",
+    event_img: "https://cdn.cnn.com/cnnnext/dam/assets/130925122807-09-kids-books-0925-horizontal-large-gallery.jpg"
+    },
+  {
+    event_id: 2,
+    event_name: "Felles vårrengjøring av fotballbanen",
+    event_description: "brabrabrabrabrabberbeewkfnefleirjglekrnfrlgjntkjtnfreregjernkjgnerkjgnrefkm v,djfenwlrkgmlrkgmvf,md r,kwelwøkflvbktmb",
+    date_starting: "12-14-2356",
+    date_ending: "12-14-2356",
+    status_fk: "AVSLUTTET",
+    location_fk: "Fotballbanen",
+    event_img: "https://proxy.duckduckgo.com/iu/?u=http%3A%2F%2Fwww.fotballbanen.com%2Fwp-content%2Fuploads%2F2017%2F10%2Fatletico-madrid.jpg&f=1"
+    },
+
+]
 
 function TabContainer(props) {
   return (
@@ -80,17 +132,17 @@ class MuniPage extends React.Component<Props, State>{
 
     return (
       <main>
-      <Grid container spacing={24}>
-        <Grid item xs={1}>
-        </Grid>
-        <Grid item xs={10}>
-          <Card className={classes.card}>
-            <CardContent>
-            <Grid container spacing={24}>
-              <Grid item md={9}>
-                <Typography variant="h3" className={classes.tittel}>{this.state.municipality}</Typography>
+        <Grid container spacing={24}>
+          <Grid item xs={1}>
+          </Grid>
+          <Grid item xs={10}>
+            <Card className={classes.cardPage}>
+              <CardContent>
+              <Grid container spacing={24}>
+                <Grid item md={9} sm={12}>
+                  <Typography variant="h3" className={classes.tittel}>{this.state.municipality}</Typography>
                 </Grid>
-                <Grid item md={3}>
+                <Grid item md={3} sm={12}>
                   <Button variant="contained" color="primary" size="large" className={classes.button} onClick={this.registerProblem}>
                     Registrer et problem
                   </Button>
@@ -107,8 +159,62 @@ class MuniPage extends React.Component<Props, State>{
                       <LinkTab variant="fullWidth" label= {<span className={classes.labeltext}>Arrangementer</span>} href="arrangement" />
                       <LinkTab variant="fullWidth" label= {<span className={classes.labeltext}>Problemer</span>} href="problemer" />
                     </Tabs>
-                  {value === 0 && <TabContainer>Arrangementer kommer her</TabContainer>}
-                  {value === 1 && <TabContainer>Problemer kommer her</TabContainer>}
+                  {value === 0 && <TabContainer>
+                    <Grid container spacing={24}>
+                    {events.map(event => (
+                        <Grid key={event.event_id} item lg={4} md={6} sm={12} sx={12}>
+                        {console.log("Events" , JSON.stringify(event))}
+
+                          <Card className={classes.card}>
+                            <CardActionArea>
+                              <CardMedia
+                                component="img"
+                                alt="Bilde av arrangement"
+                                className={classes.media}
+                                height="180"
+                                image={event.event_img}
+                                title={event.event_name}
+                              />
+                              <CardContent>
+                              <Typography component="p" className={classes.statustext}>
+                                <b>{event.status_fk}</b>
+                              </Typography>
+                                <Typography gutterBottom variant="h5" component="h2">
+                                  {event.event_name}
+                                </Typography>
+                                <Typography component="p">
+                                  {event.event_description}
+                                </Typography>
+                                <Typography component="p">
+                                  <br/>
+                                  Starter den: {event.date_starting} <br/>
+                                  Slutter den: {event.date_ending} <br/>
+                                  <br/>
+                                </Typography>
+                                <Typography component="p">
+                                  Lokasjon: {event.location_fk}
+                                </Typography>
+                              </CardContent>
+                            </CardActionArea>
+                            <CardActions>
+                              <Grid container spacing={24}>
+                                <Grid item md={8}>
+                                </Grid>
+                                <Grid item md={4}>
+                                  <Button variant="contained"  size="small" color="primary">
+                                    Kart
+                                  </Button>
+                                </Grid>
+                              </Grid>
+                            </CardActions>
+                          </Card>
+                        </Grid>
+                    ))}
+                    </Grid>
+                    </TabContainer>}
+                  {value === 1 && <TabContainer>
+                    Problemer kommer her
+                    </TabContainer>}
                 </div>
             </CardContent>
           </Card>
@@ -116,7 +222,7 @@ class MuniPage extends React.Component<Props, State>{
         <Grid item xs={1}>
         </Grid>
       </Grid>
-      </main>
+    </main>
     );
   }
 
