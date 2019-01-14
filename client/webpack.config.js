6a6a3dd175978e2d24ab0c8d8e88e0ec76a984fb
otskaug <otskaug@ntnu.no>
2019-01-14 09:49:55 +0100

    right editproblem a before merge of a/b into one

--- client/webpack.config.js
+++ client/webpack.config.js
@@ -20,9 +20,15 @@ module.exports = {
     ]
   },
   devServer: {
-    publicPath: '/',
-    contentBase: './public',
+    contentBase: path.join(__dirname, 'public'),
     compress: true,
-    port: 3000
+    port: 3000,
+    overlay: {
+      warnings: true,
+      errors: true
+    },
+    proxy: {
+      '/': 'http://localhost:3001'
+    }
   }
 };
