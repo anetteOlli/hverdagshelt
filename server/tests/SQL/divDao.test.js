231aaab262afd7622b056e8466aec914a59816ea
larssorl <larssorl@stud.ntnu.no>
2019-01-18 09:47:11 +0100

    Moved files and created problemController.test

--- server/tests/SQL/divDao.test.js
+++ server/tests/SQL/divDao.test.js
@@ -37,7 +37,7 @@ test("Testing getAllCounties from DivDao", (done) => {
   dao.getAllCounties((status,data) => {
     expect(status).toBe(200);
     expect(data.length).toBe(18);
-    expect(data[0].name).toBe("Trøndelag");
+    expect(data[0].name).toBe("Akershus");
     expect(data[14].name).toBe("Buskerud");
     done();
   })
@@ -47,7 +47,7 @@ test("Testing getMunicipalitiesByCounty from DivDao", (done) => {
   let county = "Trøndelag";
   dao.getMunicipalitiesByCounty(county,(status,data) => {
     expect(status).toBe(200);
-    expect(data.length).toBe(47);
+    expect(data.length).toBe(48);
     expect(data[2].municipality).toBe("Namsos");
     expect(data[9].municipality).toBe("Agdenes");
     done();
@@ -56,7 +56,7 @@ test("Testing getMunicipalitiesByCounty from DivDao", (done) => {
 
 test("Testing createCity from DivDao", (done) => {
   let city = "Test";
-  dao.createCity(city,(status) => {
+  dao.createCity(city,(status,data) => {
     expect(status).toBe(200);
     expect(data.affectedRows).toBe(1);
     done();
@@ -65,7 +65,7 @@ test("Testing createCity from DivDao", (done) => {
 
 test("Testing createStreet from DivDao", (done) => {
   let street = "Test";
-  dao.createStreet(street, (status) => {
+  dao.createStreet(street, (status,data) => {
     expect(status).toBe(200);
     expect(data.affectedRows).toBe(1);
     done();
