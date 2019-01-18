f33e64d4d01fdac52a1afd1c2802219f590dbfed
SindreP <sindrhpa@stud.ntnu.no>
2019-01-18 08:21:36 +0100

    axios?

---
+++ client/src/store/axios.js
@@ -0,0 +1,49 @@
+// @flow
+import axios from 'axios';
+
+export const setToken = (token: string): void => localStorage.setItem('token', token);
+export const getToken = (): string => localStorage.getItem('token') || '';
+export const clearToken = (): void => localStorage.removeItem('token');
+
+const url: string = '';
+
+export const getData = (endpoint: string) => {
+  console.log(getToken());
+  return axios.get(url + endpoint, {
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: 'Bearer' + ' ' + getToken()
+    }
+  });
+};
+
+export const postData = (endpoint: string, data: any): any => {
+  return axios.post(url + endpoint, data, {
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: 'Bearer' + ' ' + getToken()
+    }
+  });
+};
+
+export const putData = (endpoint: string, data: any): any => {
+  return axios.put(url + endpoint, data, {
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: 'Bearer' + ' ' + getToken()
+    }
+  });
+};
+
+export const deleteData = (endpoint: string): any => {
+  return axios.delete(url + endpoint, {
+    headers: {
+      'Content-Type': 'application/json',
+      Accept: 'application/json',
+      Authorization: 'Bearer' + ' ' + getToken()
+    }
+  });
+};
