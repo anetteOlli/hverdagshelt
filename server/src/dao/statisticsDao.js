ad3d9b1e082008614eb18fe389feb1486bb48eab
Erlend <erlendsu@stud.ntnu.no>
2019-01-21 14:26:26 +0100

    Merge branch 'snorre' of gitlab.stud.idi.ntnu.no:larssorl/hverdagshelt into Erlend
    # Conflicts:
    #	server/src/controllers/problemController.js
    #	server/src/routes/entrepreneurRoutes.js

---
+++ server/src/dao/statisticsDao.js
@@ -0,0 +1,30 @@
+// @flow
+const Dao = require('./dao.js');
+
+module.exports = class StatisticsDao extends Dao {
+
+/*  Statistikk:
+
+    Hvor lang tid tar det fra problemer er meldt inn til de er fikset?
+  Gjennomsnittlig {tid} for hver {kommune}, for hver {kategori}, for hver {entreprenør}, per tid.
+
+  Hvilke områder har flest problemer?
+  {problemer} til hvert område per tid
+
+Hvilke bedriftsbrukere fikser flest problemer?
+  antall {problemer} fikset for hver {entreprenør}, per tid  //eventuelt
+
+Hvilke kategorier får flest problemer?
+  antall {problemer} for hver {kategori}, per tid*/
+
+
+
+
+  getStreetProblems(json, callback) {
+    const val = [json.municipality_fk, json.county_fk];
+    super.query('SELECT * FROM problem WHERE municipality_fk = ? AND county_fk = ?', val, callback);
+  }
+
+
+
+};
