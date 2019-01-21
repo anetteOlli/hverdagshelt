48d7e8e62baa5782a9b26974c2b8c6d7da30ef64
larssorl <larssorl@stud.ntnu.no>
2019-01-21 16:43:22 +0100

    Testing my new beautiful controller test yay

--- server/tests/controllers/divController.test.js
+++ server/tests/controllers/divController.test.js
@@ -1,7 +1,19 @@
+import * as mysql from 'mysql';
+
 const DivController = require('../../src/controllers/divController');
 const runsqlfile = require('../../src/dao/SQL/runsqlfile');
 
 jest.setTimeout(30000);
+
+let pool = mysql.createPool({
+  connectionLimit: 1000000,
+  host: 'mysql',
+  user: 'root',
+  password: 'abc12345!',
+  database: 'testdb',
+  debug: false,
+  multipleStatements: true
+});
 
 beforeEach(done => {
   runsqlfile('src/dao/SQL/CREATE_TABLE.sql', pool, () => {
